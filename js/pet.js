449468a89580e7a1d42afaf2a050a9520d82c075
Jeff Bohnenblust <jb198720131@gmail.com>
2021-01-07 18:42:15 -0600

    Add files via upload

---
+++ js/pet.js
@@ -0,0 +1,13 @@
+class Pet{
+    constructor(nameA,ageA,genderA,breedA,serviceA,ownerA,phoneA,animalType){
+        this.name=nameA;
+        this.age=ageA;
+        this.gender=genderA;
+        this.breed=breedA;
+        this.service=serviceA;
+        this.owner=ownerA;
+        this.phone=phoneA;
+        this.price=0;
+        this.animalType=animalType;
+    }
+}
